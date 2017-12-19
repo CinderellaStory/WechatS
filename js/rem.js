e7de2a5fdd90d4f7fb82f16240c64b5606d24ae0
xnn <xieniannian@163.com>
2017-12-19 17:43:45 +0800

    微信分享功能

---
+++ js/rem.js
@@ -0,0 +1,12 @@
+// 手机自适应屏幕JS
+(function (win,doc){
+    if (!win.addEventListener) return;
+	var html=document.documentElement;
+	function setFont()
+	{
+		var cliWidth=html.clientWidth;
+		html.style.fontSize=100*(cliWidth/750)+'px';
+	}
+	win.addEventListener('resize',setFont,false);
+	setFont();
+})(window,document);
